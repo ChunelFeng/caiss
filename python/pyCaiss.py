d9d38dc374cb89a8a91e6950cbca312972160319
junfeng.fj <junfeng.fj@alibaba-inc.com>
2020-07-05 17:48:26 +0800

    返回值修改

--- python/pyCaiss.py
+++ python/pyCaiss.py
@@ -29,21 +29,17 @@ class PyCaiss:
         self._caiss = CDLL(path)
         self._caiss.CAISS_Environment(max_thread_size, algo_type, manage_type)
 
-
     def create_handle(self, handle):
         return self._caiss.CAISS_CreateHandle(pointer(handle))
-
 
     def init(self, handle, mode, distance_type, dim, model_path):
         path = create_string_buffer(model_path.encode(), len(model_path)+1)
         return self._caiss.CAISS_Init(handle, mode, distance_type, dim, path)
 
-
     def train(self, handle, data_path, max_data_size, normalize,
               max_index_size, precision, fast_rank, real_rank, step, max_epoch, show_span):
         return self._caiss.CAISS_Train(handle, data_path, max_data_size, normalize,
                                        max_index_size, precision, fast_rank, real_rank, step, max_epoch, show_span)
-
 
     def sync_search(self, handle, info, search_type, top_k, filter_edit_distance):
         word = create_string_buffer(info.encode(), len(info)+1)
@@ -51,7 +47,7 @@ class PyCaiss:
         if 0 != ret:
             return ''
 
-        size = c_int(0)
+        size = c_int(0)    # 获取结果大小
         ret = self._caiss.CAISS_GetResultSize(handle, byref(size))
         if 0 != ret:
             return ''
@@ -63,6 +59,5 @@ class PyCaiss:
 
         return result.value.decode()
 
-
     def destroy(self, handle):
-        self._caiss.CAISS_DestroyHandle(handle)
+        return self._caiss.CAISS_DestroyHandle(handle)

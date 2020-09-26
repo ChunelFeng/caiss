524e858b619b198e61b6c003d45b4c8d20760758
junfeng.fj <junfeng.fj@alibaba-inc.com>
2020-09-26 18:12:07 +0800

    test

---
+++ python/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+__all__ = ['pyCaissServer', 'pyCaiss']
--- python/pyCaissServer.py
+++ python/pyCaissServer.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 import json
 import sys
-import os
-
-print(os.getcwd())
 
 import tornado.ioloop
 import tornado.web

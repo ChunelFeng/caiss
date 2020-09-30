1549b934826c45f60af399a7c42df085ce61b6d9
junfeng.fj <junfeng.fj@alibaba-inc.com>
2020-09-30 23:53:09 +0800

    使用新的方法做embedding

--- python/dataProcess/pyCaissTrainDataBuilder.py
+++ python/dataProcess/pyCaissTrainDataBuilder.py
@@ -5,8 +5,8 @@ import os
 import json
 import datetime
 import codecs
-import numpy as np
 
+import numpy as np
 from keras_bert import *
 
 
@@ -25,8 +25,8 @@ def build_bert_layer(bert_path, trainable=True, training=False, seq_len=None, na
     return bert_layer
 
 
-# 通过bert构建词向量信息
-def bert_predict(data, bert_model_path):
+# 生成供caiss训练的文件
+def build_train_data(data_path, output_path, bert_model_path):
     token_dict = {}
     with codecs.open(bert_model_path + 'vocab.txt', 'r', 'utf8') as reader:
         for line in reader:
@@ -34,32 +34,22 @@ def bert_predict(data, bert_model_path):
             token_dict[token] = len(token_dict)    # 从bert的词表中读取信息
 
     tokenizer = Tokenizer(token_dict)
-    tokens = tokenizer.tokenize(data)
-
     model = build_bert_layer(bert_model_path)
-    indices, segments = tokenizer.encode(first=data, max_len=len(tokens) + 100)
-    predicts = model.predict([np.array([indices]), np.array([segments])])[0]
-    return tokens, predicts    # 返回标签信息，和预测的结果信息
-
-
-# 生成供caiss训练的文件
-def build_train_data(data_path, output_path, bert_model_path):
-    words = ''
-    with open(data_path, 'r') as fr:
-        for word in fr.readlines():
-            words = words + ' ' + word     # 读入本地词表中的词语信息
-
-    print('begin to embedding...')
-    start = datetime.datetime.now()
-    tokens, tensors = bert_predict(data=words, bert_model_path=bert_model_path)
-    print('bert embedding finished, time cost is : {0}'.format(datetime.datetime.now() - start))
 
     fw = open(output_path, 'w+')
-    for i in range(1, len(tokens)-1):    # 第一个信息是[CLS]，最后一个信息是[SEP]
-        if 0 == i % 1000:
-            print('already process [{0}] lines, [{1}] lines was left.'.format(i, len(tokens) - i))
-        result_dict = {tokens[i]: [str(tensors[i][j]) for j in range(0, len(tensors[i]))]}
-        fw.writelines(json.dumps(result_dict) + '\n')
+    with open(data_path, 'r') as fr:
+        num = 0
+        for word in fr.readlines():
+            word = word.strip('\n')
+            indices, segments = tokenizer.encode(first=word, max_len=512)
+            tensor = model.predict([np.array([indices]), np.array([segments])])[0][0]
+            result_dict = {word: [str(tensor[j])[0:8] for j in range(0, len(tensor))]}
+            fw.writelines(json.dumps(result_dict) + '\n')
+
+            num += 1
+            if 0 == num % 1000:
+                print('[caiss] bert predict {0} words, and {1} words left'.format(num, len(token_dict) - num))
+
     return
 
 
@@ -68,8 +58,8 @@ if __name__ == '__main__':
     # 开启bert服务
     print('[caiss] bert server has been started')
     bert_model_path = r'/home/chunel/model/bert_model/uncased_L-12_H-768_A-12/'    # bert模型所在的文件路径
-
     embedding_file_path = r'./doc/english-words-71290.txt'    # 获取需要处理的文档
     result_path = r'./doc/caiss_train.txt'    # 训练结束后，可供caiss训练的文件的位置
+
     # 构造可供caiss训练的文件内容
     build_train_data(embedding_file_path, result_path, bert_model_path)

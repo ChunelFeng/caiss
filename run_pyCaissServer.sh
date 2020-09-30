d2a314edc0a94b8d4e013dd1dc8d190716317303
junfeng.fj <junfeng.fj@alibaba-inc.com>
2020-09-30 22:36:18 +0800

    更新了脚本程序

--- run_pyCaissServer.sh
+++ run_pyCaissServer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "nohup run caiss server begin..."
-nohup python3 pyCaissServer.py >>output.log 2>&1 &
+nohup python3 python/pyCaissServer.py >>output.log 2>&1 &
--- run_pyCaissTrainDataBuilder.sh
+++ run_pyCaissTrainDataBuilder.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo "run caiss train data builder begin..."
-cd python/dataProcess
-python3 pyCaissTrainDataBuilder.py
+python3 python/dataProcess/pyCaissTrainDataBuilder.py
 echo "run caiss train data builder finished..."

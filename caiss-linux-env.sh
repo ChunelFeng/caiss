41e1e4c8b0eb65ab05d6e30d426dc469abfa9b60
junfeng.fj <junfeng.fj@alibaba-inc.com>
2020-11-14 18:19:14 +0800

    取消sudo指令

--- caiss-linux-env.sh
+++ caiss-linux-env.sh
@@ -5,12 +5,12 @@
 # @Desc Caiss在linux上运行需要安装的环境集合
 #**************************************************************/
 
-sudo apt-get update     # 更新软件列表
-sudo apt-get upgrade    # 更新软件
+apt-get update     # 更新软件列表
+apt-get upgrade    # 更新软件
 
-sudo apt-get install git
-sudo apt-get install cmake
-sudo apt-get install g++
-sudo apt-get install libboost-dev      # 安装boost库
-sudo apt-get install libeigen3-dev     # 安装eigen3
-sudo cp -r /usr/include/eigen3/Eigen/ /usr/local/include/
+apt-get install git
+apt-get install cmake
+apt-get install g++
+apt-get install libboost-dev      # 安装boost库
+apt-get install libeigen3-dev     # 安装eigen3
+cp -r /usr/include/eigen3/Eigen/ /usr/local/include/

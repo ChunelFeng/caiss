481ee1901b487cd247a7f50c605d8ae6629c9b3b
junfeng.fj <junfeng.fj@alibaba-inc.com>
2020-11-14 18:48:10 +0800

    添加绿色字体提示

--- caiss-linux-env.sh
+++ caiss-linux-env.sh
@@ -5,22 +5,22 @@
 # @Desc Caiss在linux上运行需要安装的环境集合
 #**************************************************************/
 
-echo "[caiss] begin to update environment, please always enter Y when needed."
+echo -e "\033[32m [caiss] begin to update environment, please always enter Y when needed. \033[0m"
 apt-get update     # 更新软件列表
 apt-get upgrade    # 更新软件
 
-echo "[caiss] auto install [git]."
+echo -e "\033[32m [caiss] auto install [git]. \033[0m"
 apt-get install git
 
-echo "[caiss] auto install [cmake]."
+echo -e "\033[32m [caiss] auto install [cmake]. \033[0m"
 apt-get install cmake
 
-echo "[caiss] auto install [g++]."
+echo -e "\033[32m [caiss] auto install [g++]. \033[0m"
 apt-get install g++
 
-echo "[caiss] auto install [boost]."
+echo -e "\033[32m [caiss] auto install [boost]. \033[0m"
 apt-get install libboost-dev      # 安装boost库
 
-echo "[caiss] auto install [eigen3]."
+echo -e "\033[32m [caiss] auto install [eigen3]. \033[0m"
 apt-get install libeigen3-dev     # 安装eigen3
 cp -r /usr/include/eigen3/Eigen/ /usr/local/include/

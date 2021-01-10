141b8bebe7d52594f8c3609c72c4eaa89e95da6b
junfeng.fj <junfeng.fj@alibaba-inc.com>
2021-01-10 16:36:24 +0800

    更新安装信息

--- caiss-linux-env.sh
+++ caiss-linux-env.sh
@@ -10,22 +10,22 @@ echo -e "\033[32m [caiss] begin to update environment, please always enter [Y] i
 apt-get update     # 更新软件列表
 apt-get upgrade    # 更新软件
 
-echo -e "\033[32m [caiss] begin to install [git], please enter Y if needed. \033[0m"
+echo -e "\033[32m [caiss] begin to install [git], please enter [Y] if needed. \033[0m"
 apt-get install git
 
-echo -e "\033[32m [caiss] begin to install [cmake], please enter Y if needed. \033[0m"
+echo -e "\033[32m [caiss] begin to install [cmake], please enter [Y] if needed. \033[0m"
 apt-get install cmake
 
-echo -e "\033[32m [caiss] begin to install [g++], please enter Y if needed. \033[0m"
+echo -e "\033[32m [caiss] begin to install [g++], please enter [Y] if needed. \033[0m"
 apt-get install g++
 
-echo -e "\033[32m [caiss] begin to install [boost], please enter Y if needed. \033[0m"
+echo -e "\033[32m [caiss] begin to install [boost], please enter [Y] if needed. \033[0m"
 apt-get install libboost-dev      # 安装boost库
 
-echo -e "\033[32m [caiss] begin to install [eigen3], please enter Y if needed. \033[0m"
+echo -e "\033[32m [caiss] begin to install [eigen3], please enter [Y] if needed. \033[0m"
 apt-get install libeigen3-dev     # 安装eigen3
 cp -r /usr/include/eigen3/Eigen/ /usr/local/include/
 
-echo -e "\033[32m [caiss] congratulation! U have been finished install environment all Caiss needed on Linux. \033[0m"
+echo -e "\033[32m [caiss] congratulation! U have been finished installing environment all Caiss needed on Linux. \033[0m"
 echo -e "\033[32m [caiss] please go on as README described. \033[0m"
 echo -e "\033[32m ************************************************************** \033[0m"
